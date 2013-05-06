1b1f5483ebb2a0d305e73c095b4c74b72863d604
Forest Geng <gengfo@oocl.com>
2013-05-06 14:12:27 +0800

    deleted

--- JavaDesignPattern/backup/src/Composite/Sample/FileTreatmentException.java
+++
@@ -1,7 +0,0 @@
-public class FileTreatmentException extends RuntimeException {      // RuntimeExceptionÇ≈ê≥ÇµÇ¢Ç©ÅH
-    public FileTreatmentException() {
-    }
-    public FileTreatmentException(String msg) {
-        super(msg);
-    }
-}
